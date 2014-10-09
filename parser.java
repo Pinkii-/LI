6d12c9910bee7fe6697841ac1fa32055051967c0
Pinkii- <pinkiibiza@gmail.com>
2014-10-09 20:50:33 +0200

    parser rico, parser bueno

--- parser.java
+++ parser.java
@@ -9,27 +9,40 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
  
+public enum Line {
+  NAME,PTIME,PDEC,PPROPAG,
+       MTIME,MDEC,MPROPAG
+};
+
 public class parser
 {
-    public static void main(String[] args) throws FileNotFoundException, IOException
-    {
-        InputStream in = new FileInputStream(new File(args[0]));
-        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-        String line;
-        ArrayList<ArrayList<String> > info = new ArrayList<ArrayList<String> >();
-        int n = 0;
-        while ((line = reader.readLine()) != null) {
-              
-            
-            String delims = "[ ]+"; // use + to treat consecutive delims as one;
+  public static void main(String[] args) throws FileNotFoundException, IOException  {
+    InputStream in = new FileInputStream(new File(args[0]));
+    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+    String line;
+    ArrayList<ArrayList<String>> info = new ArrayList<ArrayList<String>>();
+    int n = 0;
+    while ((line = reader.readLine()) != null) {
+      info.add(new ArrayList<String>());
+         
+      String delims = "[ ]+"; // use + to treat consecutive delims as one;
                                  // omit to treat consecutive delims separately
-            String[] tokens = line.split(delims);
-            for (int i = 0; i < tokens.length; ++i) info[n][i]= tokens[i];
-            System.out.println(tokens[0]+" "+tokens[1]+" "+" "+tokens[2]+" "+" "+tokens[3]+" "+" "+tokens[4]+" "+" "+tokens[5]+" ");   //Prints the string content read from input stream      
- 
-            ++n;            
-        }
+      String[] tokens = line.split(delims);
+      for (int i = 0; i < tokens.length; ++i) { 
+		    info.get(n).add(tokens[i]);
+		
+       	System.out.print(info.get(n).get(i)+" ");   //Prints the string content read from input stream   
+      }	   
+ 	    System.out.println();
+      ++n;            
+      }
+
+      for(int i = 0; i < info.size(); ++i) {
+        System.out.println("----------------------------------------------------------------------------------");
+        System.out.println(info.get(i).get(NAME));
+        System.out.println();
+      } 
         
-        reader.close();
+      reader.close();
     }
 }
